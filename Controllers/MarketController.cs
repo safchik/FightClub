828c089352650f8a3351311e10f1f5355e006867
Dmytro <sagchik9@gmial.com>
2025-10-02 22:28:37 +0100

    working on market view

--- Controllers/MarketController.cs
+++ Controllers/MarketController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FightClub.Data;
+using FightClub.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FightClub.Controllers
 {
     public class MarketController : Controller
     {
+        private readonly FightClubDbContext _context;
+        public MarketController(FightClubDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Item> items = _context.Items.ToList();
+            return View(items);
         }
     }
 }
