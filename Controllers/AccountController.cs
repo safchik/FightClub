d1dce3a5234a3e0a5a835925e37275c6fdd2f2e3
Dmytro <sagchik9@gmial.com>
2025-10-11 19:48:29 +0100

    login register functionallity added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using FightClub.Data;
+using FightClub.Models;
+using FightClub.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightClub.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<Player> _userManager;
+        private readonly SignInManager<Player> _signInManager;
+        private readonly FightClubDbContext _context;
+
+        public AccountController(UserManager<Player> userManager, SignInManager<Player> signInManager, FightClubDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Home");
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new Player
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                Name = model.Name,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    
+    }
+}
