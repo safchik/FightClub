8b78c073476fd628fa58fe3b1dd2ee22fc74fd71
Dmytro <sagchik9@gmial.com>
2025-10-13 11:14:04 +0100

    corrected character creation process

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using FightClub.Data;
+using FightClub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FightClub.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly FightClubDbContext _context;
+        private readonly UserManager<Player> _userManager;
+        public ProfileController(FightClubDbContext context, UserManager<Player> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null) 
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var characters = await _context.Characters
+                .Where(c => c.PlayerId == user.Id)
+                .ToListAsync();
+
+            ViewBag.Player = user;
+            return View(characters);
+        }
+    }
+}
