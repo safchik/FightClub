44bc2679d3e7e85c5a1ccdb48d73af90d2655cf2
Dmytro <sagchik9@gmial.com>
2025-10-18 23:41:24 +0100

    handle choosing character migration added, controller logic implemented, simple view added

--- Migrations/20251018220155_ConfigurePlayerCharacterRelationship.Designer.cs
+++ Migrations/20251018220155_ConfigurePlayerCharacterRelationship.Designer.cs
@@ -4,6 +4,7 @@ using FightClub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FightClub.Migrations
 {
     [DbContext(typeof(FightClubDbContext))]
-    partial class FightClubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251018220155_ConfigurePlayerCharacterRelationship")]
+    partial class ConfigurePlayerCharacterRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251018220155_ConfigurePlayerCharacterRelationship.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FightClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigurePlayerCharacterRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ActiveCharacterId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ActiveCharacterId",
+                table: "AspNetUsers",
+                column: "ActiveCharacterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Characters_ActiveCharacterId",
+                table: "AspNetUsers",
+                column: "ActiveCharacterId",
+                principalTable: "Characters",
+                principalColumn: "CharacterId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Characters_ActiveCharacterId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ActiveCharacterId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ActiveCharacterId",
+                table: "AspNetUsers");
+        }
+    }
+}
